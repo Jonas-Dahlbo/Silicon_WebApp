df4102990d9dd61effeaddcb57ac9eb8560a5f5c
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-04-20 12:34:37 +0200

    Added some ViewModels changed

---
+++ WebApp/Models/Views/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using WebApp.Models.Sections;
+
+namespace WebApp.Models.Views;
+
+public class HomeIndexViewModel
+{
+    public string Title { get; set; } = "";
+    public ShowcaseViewModel Showcase { get; set; }
+}
