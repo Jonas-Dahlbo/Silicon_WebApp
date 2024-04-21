2b2184dfe1ae360190e1b902726a8c224308975a
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-04-21 23:24:02 +0200

    Finished most of signin and signup

---
+++ WebApp/ViewModels/Sections/SignInViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Filters;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels.Sections;
+
+public class SignInViewModel
+{
+
+    [Display(Name = "Email address", Prompt = "Enter your email address")]
+    [Required(ErrorMessage = "A valid Email Address must be entered")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "********")]
+    [Required(ErrorMessage = "The correct Password must be entered")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Remember me")]
+    public bool RememberMe { get; set; }
+}
