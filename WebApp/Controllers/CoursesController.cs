be6774836bac0c0d7ac69e5c9dc3b685a7881b0e
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-04-24 22:59:21 +0200

    fixed signed in

--- WebApp/Controllers/CoursesController.cs
+++ WebApp/Controllers/CoursesController.cs
@@ -1,19 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using WebApp.Models.Views;
 using WebApp.ViewModels.Views;
 
 namespace WebApp.Controllers
 {
-    public class CoursesController : Controller
+    public class CoursesController(SignInManager<UserEntity> signInManager) : Controller
     {
+        private readonly SignInManager<UserEntity> _signInManager = signInManager;
+
         [HttpGet]
         [Route("/courses")]
         public IActionResult Index()
         {
-            var viewModel = new CoursesViewModel();
+            if (!ModelState.IsValid)
+            {
+                if (!_signInManager.IsSignedIn(User))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                var viewModel = new CoursesViewModel();
 
-            ViewData["Title"] = viewModel;
-            return View(viewModel);
+                ViewData["Title"] = viewModel;
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
